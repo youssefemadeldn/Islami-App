f0be25e58e6f683a05fa1ed467af3a4d32d497f4
youssef emad eldin <youssefemadeldn>
2024-07-17 16:35:36 +0300

    trigger the provider => 4.1

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,7 @@ import 'package:islamic_app/views/quran_content_view.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,19 +16,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ThemeProvider provider = Provider.of<ThemeProvider>(context);
-    return MaterialApp(
-      themeMode: provider.mode,
-      theme: lightTheme(),
-      darkTheme: darkTheme(),
-      debugShowCheckedModeBanner: false,
-      initialRoute: BottomNavigationBarView.routName,
-      routes: {
-        BottomNavigationBarView.routName: (context) =>
-            BottomNavigationBarView(),
-        HadithContentView.routName: (context) => HadithContentView(),
-        QuranContentView.routName: (context) => QuranContentView(),
-      },
+    // ThemeProvider provider = Provider.of<ThemeProvider>(context);
+    return ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      builder: (context, child) => MaterialApp(
+        themeMode: Provider.of<ThemeProvider>(context).mode,
+        theme: lightTheme(),
+        darkTheme: darkTheme(),
+        debugShowCheckedModeBanner: false,
+        initialRoute: BottomNavigationBarView.routName,
+        routes: {
+          BottomNavigationBarView.routName: (context) =>
+              BottomNavigationBarView(),
+          HadithContentView.routName: (context) => HadithContentView(),
+          QuranContentView.routName: (context) => QuranContentView(),
+        },
+      ),
     );
   }
 }
