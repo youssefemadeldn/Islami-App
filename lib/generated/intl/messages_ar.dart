6f54e9404c55905bdb03b7acba2883fac2ad78d1
youssef emad eldin <youssefemadeldn>
2024-07-21 17:58:11 +0300

    fill json files with translation  => 6

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -21,5 +21,19 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ar';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "ahadith": MessageLookupByLibrary.simpleMessage("الأحاديث"),
+        "arabic": MessageLookupByLibrary.simpleMessage("العربية"),
+        "dark": MessageLookupByLibrary.simpleMessage("داكن"),
+        "english": MessageLookupByLibrary.simpleMessage("الإنجليزية"),
+        "islami": MessageLookupByLibrary.simpleMessage("إسلامي"),
+        "language": MessageLookupByLibrary.simpleMessage("اللغة"),
+        "light": MessageLookupByLibrary.simpleMessage("فاتح"),
+        "number_of_praises":
+            MessageLookupByLibrary.simpleMessage("عدد التسبيحات"),
+        "quran_radio":
+            MessageLookupByLibrary.simpleMessage("إذاعة القرآن الكريم"),
+        "sura_name": MessageLookupByLibrary.simpleMessage("إسم السورة"),
+        "theme": MessageLookupByLibrary.simpleMessage("الثيم")
+      };
 }
--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -21,5 +21,18 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "ahadith": MessageLookupByLibrary.simpleMessage("Ahadith"),
+        "arabic": MessageLookupByLibrary.simpleMessage("Arabic"),
+        "dark": MessageLookupByLibrary.simpleMessage("Dark"),
+        "english": MessageLookupByLibrary.simpleMessage("English"),
+        "islami": MessageLookupByLibrary.simpleMessage("Islami"),
+        "language": MessageLookupByLibrary.simpleMessage("Language"),
+        "light": MessageLookupByLibrary.simpleMessage("Light"),
+        "number_of_praises":
+            MessageLookupByLibrary.simpleMessage("Number of Praises"),
+        "quran_radio": MessageLookupByLibrary.simpleMessage("Quran Radio"),
+        "sura_name": MessageLookupByLibrary.simpleMessage("Sura Name"),
+        "theme": MessageLookupByLibrary.simpleMessage("Theme")
+      };
 }
